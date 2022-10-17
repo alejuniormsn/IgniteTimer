2cbd811be1b37bc361ece77db4b2771f17e1bd95
Alexandre Junior <alejunior@msn.com>
2022-10-17 19:59:51 -0300

    componente timer

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer } from "./styles";
+import logoIgnite from '../../assets/Logo.svg';
+import { Scroll, Timer } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="logo do ignite" />
+      <nav>
+        <NavLink to='/' end={true} title="Timer">
+          <Timer size={24}/>
+        </NavLink>
+        <NavLink to='/history' end={true} title="Histórico">
+          <Scroll size={24}/>
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
